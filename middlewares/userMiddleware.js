ac508589b8d6bde8783d5916172d4405ab55f25b
vilmatiainen <vilma.tiainen@aalto.fi>
2021-12-08 12:55:18 +0200

    renamed a variable in userMiddleware

--- middlewares/userMiddleware.js
+++ middlewares/userMiddleware.js
@@ -4,8 +4,8 @@ const userMiddleware = async (context, next) => {
     const user = await context.state.session.get("user");
   
     if (user) {
-      const userFromDatabase = await userService.findByEmail(user.email);
-      context.user = userFromDatabase[0];
+      const user = await userService.findByEmail(user.email);
+      context.user = user[0];
     };
   
     await next();
